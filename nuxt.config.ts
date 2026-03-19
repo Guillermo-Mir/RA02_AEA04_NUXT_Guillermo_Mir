468f7e04483b724d2149890058ee12b88b6c3e6f
Guillermo-Mir <guillermo.mir.7e8@itb.cat>
2026-03-19 17:39:12 +0100

    modificacions nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+/*
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -15,4 +16,42 @@ export default defineNuxtConfig({
       },
     },
   },
+});
+*/
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-auth-utils"],
+  runtimeConfig: {
+    oauth: {
+      github: {
+        clientId: "",
+        clientSecret: "",
+      },
+    },
+  },
+  routeRules: {
+    '/api/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost', 
+        'Access-Control-Allow-Credentials': 'true', 
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization'
+      }
+    },
+    '/auth/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost', 
+        'Access-Control-Allow-Credentials': 'true', 
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization'
+      }
+    }
+  }
 });
